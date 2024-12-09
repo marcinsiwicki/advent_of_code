129046f986f021991f08ba8036c4e3e57a49952e
Marcin Siwicki <marcin.siwicki@gmail.com>
2024-12-08 21:43:39 -0600

    new space intense solution rather than loop only

--- 2024/day07/puzzle7.py
+++ 2024/day07/puzzle7.py
@@ -3,43 +3,25 @@ Advent of Code 2024: Day 7
 """
 import math
 import itertools
+import time
+from line_profiler import profile
 
 
 def part1(input_eqs: dict) -> int:
     """Determine if + or * operators can be used to achieve result."""
-
-    operators = {'+': sum,
-                 '*': math.prod
-                 }
-
     total_calibrations = 0
     no_match = {}
     for key, vals in input_eqs.items():
-        valid_match = False
-
-        if sum(vals) == key: # all sums
+        w = [[vals[0]]]
+        for i in vals[1:]:
+            next_iteration = []
+            for j in w[-1]:
+                next_iteration.append(j + i)
+                next_iteration.append(j * i)
+            w.append(next_iteration)
+        if key in w[-1]:
             total_calibrations += key
-            valid_match = True
-        elif math.prod(vals) == key: # all prods
-            total_calibrations += key
-            valid_match = True
-        else: # mixed
-            # work left to right and try operators
-            n_ops = len(vals) - 1
-            cartesian = list(itertools.product(['+', '*'], repeat=n_ops))
-            cartesian = [list(l) for l in cartesian]
-            for seq in cartesian:
-                if seq == ['*'] * n_ops or seq == ['+'] * n_ops:
-                    continue
-                w = vals[0]
-                for n in vals[1:]:
-                    w = operators[seq.pop()]([w, n])
-                if w == key:
-                    total_calibrations += key
-                    valid_match = True
-                    break
-
-        if not valid_match:
+        else:
             no_match[key] = vals
 
     return total_calibrations, no_match
@@ -48,26 +30,18 @@ def part1(input_eqs: dict) -> int:
 def part2(input_eqs: dict) -> int:
     """Adding a new operator."""
 
-    def pipe(n):
-        return int(str(n[0]) + str(n[1]))
-
-    operators = {'+': sum,
-                 '*': math.prod,
-                 '||': pipe
-                 }
-
     total_calibrations = 0
     for key, vals in input_eqs.items():
-        n_ops = len(vals) - 1
-        cartesian = list(itertools.product(['+', '*', '||'], repeat=n_ops))
-        cartesian = [list(l) for l in cartesian]
-        for seq in cartesian:
-            w = vals[0]
-            for n in vals[1:]:
-                w = operators[seq.pop()]([w, n])
-            if w == key:
-                total_calibrations += key
-                break
+        w = [[vals[0]]]
+        for i in vals[1:]:
+            next_iteration = []
+            for j in w[-1]:
+                next_iteration.append(j + i)
+                next_iteration.append(j * i)
+                next_iteration.append(int(str(j) + str(i)))
+            w.append(next_iteration)
+        if key in w[-1]:
+            total_calibrations += key
 
     return total_calibrations
 
@@ -81,8 +55,10 @@ if __name__ == '__main__':
         res, inputs = l.split(':')[0], l.split(':')[1]
         equations[int(res)] = [int(i) for i in inputs.split()]
 
+    s = time.time()
     cal_sum, no_match = part1(equations)
-    print(f"+ * : {cal_sum}")
+    print(f"+ * : {cal_sum} in {time.time() - s}")
 
+    s = time.time()
     addl_sum = part2(no_match)
-    print(f"+ * || : {cal_sum + addl_sum}")
+    print(f"+ * || : {cal_sum + addl_sum} in {time.time() - s}")
